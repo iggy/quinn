f4e1434d994ec776f18581190bcc6462bd1dd66c
Brian Jackson <iggy@theiggy.com>
2009-11-17 18:53:25 -0600

    netscan auto-tagging, new snmptraprcv plugin
    netscan() will now add a tag for AD domain controllers.
    
    added anyjson to the project files so it doesn't have to be installed separately
    
    new plugin that receives snmptraps, doesn't do anything with the data it
    receives yet

---
+++ server/plugins/snmptraprcv.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import dbus, gobject, socket
+from quinn.server.plugins import QuinnPlugin
+
+from pysnmp.entity import engine, config
+from pysnmp.carrier.asynsock.dgram import udp
+from pysnmp.entity.rfc3413 import ntfrcv
+
+class SnmptraprcvPlugin(QuinnPlugin):
+    """create a control socket and listen for commands coming in on it"""
+    def __init__(self):
+        self.snmpEngine = engine.SnmpEngine()
+        
+        config.addSocketTransport(
+            self.snmpEngine,
+            udp.domainName,
+            udp.UdpSocketTransport().openServerMode(('0.0.0.0', 162))
+            )
+            
+        config.addV1System(self.snmpEngine, 'test-agent', 'public')
+        
+        config.addV3User(
+            self.snmpEngine, 'test-user',
+            config.usmHMACMD5AuthProtocol, 'authkey1',
+            config.usmDESPrivProtocol, 'privkey1'
+            #    '80004fb81c3dafe69'   # ContextEngineID of Notification Originator
+            )
+        # Apps registration
+        ntfrcv.NotificationReceiver(self.snmpEngine, self.recvcallback)
+        self.snmpEngine.transportDispatcher.jobStarted(1) # this job would never finish
+        self.snmpEngine.transportDispatcher.runDispatcher()
+
+
+    def recvcallback(self, snmpEngine,
+        contextEngineId, contextName,
+        varBinds,
+        cbCtx):
+        print 'Notification from SNMP Engine \"%s\", Context \"%s\"' % (
+            contextEngineId, contextName
+        )
+        for name, val in varBinds:
+            print '%s = %s' % (name.prettyPrint(), val.prettyPrint())
+
